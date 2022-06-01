56caef065b790609e3e3f93063974cb817ba5309
divami-praveen <praveen@divami.com>
2022-06-01 16:14:16 +0530

    updated sid

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ import { NestorReporter } from "./NestorReporter";
 import "./styles.css";
 import { consolidateProjData, flattenProjData } from "./utils";
 
-
-const sid = "h700c2qthusq2itics4covhgs1";
+const sid = "4sol5gevppn3dfqu864k7v2287";
 const reqUrl = "https://nestor.divami.com/request.php";
 export default function App() {
-
   const [projectsList, setProjectsList] = useState([]);
   const [selectedProject, setSelectedProject] = useState([]);
   const [selectedProjectData, setSelectedProjectData] = useState([]);
   const [OverallDataState, setOverallDataState] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
 
   const getTimeData = async (tempSelectProj) => {
     try {
@@ -28,8 +25,8 @@ export default function App() {
           projectId: tempSelectProj.projectId, // "1"
           startDate: "2016-01-01",
           endDate: moment().format("YYYY-MM-DD"),
-          individualProject: true
-        }
+          individualProject: true,
+        },
       });
       if (resTime.data && resTime.data.data) {
         const tempProjData = Array.from(resTime.data.data);
@@ -50,7 +47,7 @@ export default function App() {
       const res = await axios.post(reqUrl, {
         command: "getProjectList",
         client: { sid },
-        data: { count: 999 }
+        data: { count: 999 },
       });
       if (res.data && res.data.data && res.data.data?.projects) {
         const projects = Array.from(res?.data?.data?.projects);
@@ -73,61 +70,57 @@ export default function App() {
 
       const AllRes = await Promise.all(
         projectsList
-          .filter(
-            x =>
-              x.status === "Active" && x.projectId !== "1"
+          .filter((x) => x.status === "Active" && x.projectId !== "1")
+          .map((prj) =>
+            axios.post(reqUrl, {
+              command: "getProjectTimesheet",
+              client: { sid },
+              data: {
+                projectId: prj.projectId, // "1"
+                startDate: "2000-01-01",
+                endDate: moment().format("YYYY-MM-DD"),
+                individualProject: true,
+              },
+            })
           )
-          .map(
-            prj =>
-              axios.post(reqUrl, {
-                command: "getProjectTimesheet",
-                client: { sid },
-                data: {
-                  projectId: prj.projectId, // "1"
-                  startDate: "2000-01-01",
-                  endDate: moment().format("YYYY-MM-DD"),
-                  individualProject: true
-                }
-              })))
+      );
       let overallDataTemp = projectsList
-        .filter(
-          x =>
-            x.status === "Active" && x.projectId !== "1"
-        ).map((proj, index) => {
-          let prjdata = AllRes[index] &&
-            AllRes[index].data &&
-            AllRes[index].data.data ?
-            AllRes[index].data.data :
-            []
+        .filter((x) => x.status === "Active" && x.projectId !== "1")
+        .map((proj, index) => {
+          let prjdata =
+            AllRes[index] && AllRes[index].data && AllRes[index].data.data
+              ? AllRes[index].data.data
+              : [];
           prjdata = flattenProjData(prjdata);
           prjdata = consolidateProjData(prjdata);
           return {
             ...proj,
-            data: prjdata
-          }
-        })
+            data: prjdata,
+          };
+        });
 
       // console.log(overallDataTemp)
-      setOverallDataState(overallDataTemp)
+      setOverallDataState(overallDataTemp);
       setIsLoading(false);
     } catch (err) {
       setIsLoading(false);
       console.log("getOverallData Error=>", err);
     }
-  }
+  };
 
   useEffect(() => {
-    if (projectsList.length > 0) { getOverallData().catch() }
-    var list = document.getElementById("root").parentElement.children
-    for (var i = 0; i < list.length; i++) {
-      if (list[i].localName === "iframe") list[i].remove()
+    if (projectsList.length > 0) {
+      getOverallData().catch();
     }
-  }, [projectsList])
+    var list = document.getElementById("root").parentElement.children;
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].localName === "iframe") list[i].remove();
+    }
+  }, [projectsList]);
 
   useEffect(() => {
     getOrgData().catch();
-  }, [])
-
+  }, []);
 
   return (
     <div className="App">
